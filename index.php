4f05d12d66030ca155814efd201df72d8d46c228
Pera030 <pericapaunovic@gmail.com>
2021-05-21 23:16:04 +0200

    prva promena

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content=width=device-width,initial-scale=1.0>
+    <meta name="author" content="Perica Paunovic">
+    <title>Bussiness practice 2</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;700&display=swap" rel="stylesheet"> <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400&display=swap" rel="stylesheet"> 
+</head>
+<body>
+    
+<!-- Navigation -->
+    <nav>
+        <a href="index.php" class="logo">Business <span>Marketing &copy; consulting</span></a>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Sign up</a></li>
+            <li><a href=""><i class="fas fa-search"></i></a></li>
+        </ul>
+    </nav>
+    
+<!--  Header  -->
+    <header>
+        <div class="container">
+            <h1>Trusted and <br> professional advisers</h1>
+            <h4>For your business</h4>
+            <p>Showcase your Profile to the world using online CV builder and Get Hired Faster</p>
+            <button type="button">About us</button>
+        </div>
+    </header>
+    
+<!--  Headers  -->
+    <section class="container headings">
+        <div class="col">
+            <h2>01</h2>
+        </div>
+        <div class="col">
+            <h3>About us</h3>
+            <h2><span>We can</span> HELP YOUR BUSSINESS GROW</h2>
+            <h4>Get your business in 4 ease steps</h4>
+        </div>
+    </section>
+    
+<!--  About  -->
+    <section class="about">
+        <div class="col"></div>
+        <div class="col">
+            <h2>EVERYTHING YOU NEED IN ONE SOLUTION</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque debitis dolore, accusamus, ab quos obcaecati temporibus, distinctio voluptatem, magnam libero quibusdam? Ducimus sunt laborum officiis. Aperiam nisi, nulla minus assumenda!</p>
+        </div>
+    </section>
+    
+<!--  Headers  -->
+    <section class="container headings">
+        <div class="col">
+            <h2>02</h2>
+        </div>
+        <div class="col">
+            <h3>About us</h3>
+            <h2><span>We can</span> HELP YOUR BUSSINESS GROW</h2>
+            <h4>Get your business in 4 ease steps</h4>
+        </div>
+    </section>    
+    
+<!--  Services  -->
+    <section class="services container">
+        <div class="col">
+            <i class="fas fa-bullhorn"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo nesciunt molestiae ut, ipsum. Repellendus consequuntur nulla alias. Excepturi, consectetur ducimus, nemo tenetur, corrupti eligendi magni maiores voluptatum adipisci ullam magnam.</p>
+        </div>
+        <div class="col">
+            <i class="far fa-chart-bar"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo nesciunt molestiae ut, ipsum. Repellendus consequuntur nulla alias. Excepturi, consectetur ducimus, nemo tenetur, corrupti eligendi magni maiores voluptatum adipisci ullam magnam.</p>
+        </div>
+        <div class="col">
+            <i class="fas fa-bullseye"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo nesciunt molestiae ut, ipsum. Repellendus consequuntur nulla alias. Excepturi, consectetur ducimus, nemo tenetur, corrupti eligendi magni maiores voluptatum adipisci ullam magnam.</p>
+        </div>
+        <div class="col">
+            <i class="fas fa-map-signs"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo nesciunt molestiae ut, ipsum. Repellendus consequuntur nulla alias. Excepturi, consectetur ducimus, nemo tenetur, corrupti eligendi magni maiores voluptatum adipisci ullam magnam.</p>
+        </div>
+        <div class="col">
+            <i class="fas fa-filter"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo nesciunt molestiae ut, ipsum. Repellendus consequuntur nulla alias. Excepturi, consectetur ducimus, nemo tenetur, corrupti eligendi magni maiores voluptatum adipisci ullam magnam.</p>
+        </div>
+        <div class="col">
+            <i class="fas fa-hand-holding-usd"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo nesciunt molestiae ut, ipsum. Repellendus consequuntur nulla alias. Excepturi, consectetur ducimus, nemo tenetur, corrupti eligendi magni maiores voluptatum adipisci ullam magnam.</p>
+        </div>
+    </section>
+    
+<!--  Personal Business  -->
+    <section class="personaBusiness">
+    <!--  Headers  -->
+        <section class="container headings">
+            <div class="col">
+                <h2>03</h2>
+            </div>
+            <div class="col">
+                <h3>What we done</h3>
+                <h2><span>Create</span> PERSONALISED BUSINESS</h2>
+                <h4>Get your business in 4 ease steps</h4>
+                <button type="button">About us</button>
+            </div>
+        </section> 
+        <section class="counter container">
+            <div class="col">
+                <h2>18</h2>
+                <h4>Nominations</h4>
+                <hr>
+            </div>
+            <div class="col">
+                <h2>5</h2>
+                <h4>Awards</h4>
+                <hr>
+            </div>
+            <div class="col">
+                <h2>45</h2>
+                <h4>Agencies</h4>
+                <hr>
+            </div>
+        </section> 
+    </section>
+    
+<!--  Headers  -->
+        <section class="container headings">
+            <div class="col">
+                <h2>04</h2>
+            </div>
+            <div class="col">
+                <h3>Our team</h3>
+                <h2><span>WE HAVE PROFESSIONAL</span> TEAM OF BUSINESS ANALYSES</h2>
+                <h4>Get your business in 4 ease steps</h4>
+            </div>
+        </section>     
+
+<!--  Team  -->
+    <section class="team container">
+        <div class="col">
+            <img src="img/team1.jpg" alt="">
+            <h4>James Douglas</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi ad, eaque. Laboriosam nulla possimus quasi.</p>
+        </div>
+        <div class="col">
+            <img src="img/team2.jpg" alt="">
+            <h4>Peter Green</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi ad, eaque. Laboriosam nulla possimus quasi.</p>
+        </div>
+        <div class="col">
+            <img src="img/team3.jpg" alt="">
+            <h4>Kimi Raikonen</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi ad, eaque. Laboriosam nulla possimus quasi.</p>
+        </div>
+    </section>
+    
+<!--  Footer  -->
+    <footer>
+        <div class="container">
+            <div class="col">
+                <a href="index.php" class="logo">Business <span>Marketing &copy; consulting</span></a>
+            </div>
+            <div class="col">
+                <i class="fas fa-map-marker-alt"></i>
+                <p>Main street 2, Novi Sad</p>
+                <p>Serbia</p>
+            </div>
+            <div class="col">
+                <i class="fas fa-envelope"></i>
+                <p>info@marketing.com</p>
+                <p>office@marketing.com</p>
+            </div>
+            <div class="col">
+                <i class="fas fa-phone"></i>
+                <p>021 456 789</p>
+                <p>065/2100-118</p>
+            </div>
+        </div>
+    </footer>
+    
+</body>
+</html>
